73a14f8ac27ba796c07b052b76505eec71921874
nishantvekariya1 <nishantvekariya623@gmail.com>
2024-01-06 12:33:53 +0530

    add pre production changes

--- api/index.js
+++ api/index.js
@@ -4,13 +4,22 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/userroute.js';
 import authRoutes from './routes/authroute.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
 .then(() => {console.log("Connected to MongoDB");})
 .catch((error) => {console.log(error);})
 
+const __dirname = path.resolve();
+
 const app = express();
+
+app.use(express.static(path.join(__dirname,'./client/dist')))
+
+app.get('*',(req,res) => {
+    res.sendFile(path.join(__dirname,'client','dist','index.html'));
+});
 
 app.use(express.json());
 

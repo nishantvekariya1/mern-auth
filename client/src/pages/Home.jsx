8ba7ab0614e9c2c278b37b0e9ab47c38ea0180fd
nishantvekariya1 <nishantvekariya623@gmail.com>
2024-01-06 12:24:35 +0530

    complete home and about pages

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,11 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-700'>Welcome to my Auth App!</h1>
+      <p className='mb-4 text-slate-500'>Introducing our MERN Stack Authentication Web Application, a cutting-edge platform engineered for seamless user authentication. Leveraging the power of MongoDB, Express.js, React, and Node.js, our application provides a straightforward yet secure experience for users. The registration process ensures data confidentiality through password encryption, while the login functionality employs authentication tokens and sessions for robust user verification. With an intuitive user interface built on React.js, personalized user profiles, and industry-standard security practices, our MERN Stack Authentication Web Application guarantees a smooth and secure authentication experience.</p>
+
+      <p className='mb-4 text-slate-500'>Users can effortlessly create accounts, log in securely, and manage their profiles with ease. The responsive design ensures a consistent and enjoyable experience across various devices. The backend, powered by Node.js and Express.js, offers scalability, while MongoDB serves as a reliable database for efficient data storage. Whether you are a developer seeking a comprehensive MERN stack authentication example or a user looking for a reliable platform, our application combines simplicity and security for a seamless authentication journey. Experience the future of web authentication with our MERN Stack solution.</p>
+    </div>
   )
 }
